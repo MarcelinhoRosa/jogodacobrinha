3f60afeb960bb4b6916c6ed94a17434b6fb2b49d
unknown <lelo.c.inf@gmail.com>
2021-05-03 10:20:47 -0300

    adicionar a estilização ao body

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function criarBG() {
 }
 
 function criarCobrinha() {
-    for(i=0; i < snake.length; i++) {
+    for(i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -31,23 +31,23 @@ function  drawFood() {
 
 document.addEventListener('keydown', update);
 
-function update (event) {
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "downt") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+function update(event) {
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'downt') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 
 function iniciarJogo() {
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y =0;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     for(i = 1; i < snake.length; i++) {
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-            clearInterval(jogo0);
+            clearInterval(jogo);
             alert('Game Over :(');
         }
     }
@@ -78,7 +78,6 @@ function iniciarJogo() {
     }
 
     snake.unshift(newHead);
-
 }
 
 let jogo = setInterval(iniciarJogo, 100);
